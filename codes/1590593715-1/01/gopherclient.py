35f461650e67d52d45cae20a0dda61f8daf2e51c
lhz <lhz1210@bupt.edu.cn>
2021-04-22 20:08:30 +0800

    gopherclient

--- codes/1590593715-1/01/gopherclient.py
+++ codes/1590593715-1/01/gopherclient.py
@@ -8,6 +8,7 @@ host = sys.argv[1]   # sys.argv[]从程序外部获取参数
 filename = sys.argv[2]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 注意端口
 # 也可以直接写 s = socket.socket() 默认参数 (鼠标指向不动，会出现形参)
 # 其中参数family: socket.AF_INET 用于服务器与服务器之间的网络通信， socket.AF_INET6
 # 另一个参数type套接字类型: socket.SOCK_STREAM 用于TCP流式通信，socket.SOCK_DGRAM 用于UDP数据报式通信
